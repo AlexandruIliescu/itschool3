049cdccac9a6fbb845d7cf76ec53d82519616898
Alex <alexandru.iliescu@fortechproducts.ro>
2023-09-08 19:38:07 +0300

    Added homework

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
         //this is a comment from session2
+
+        //this is the homework
     }
 }
